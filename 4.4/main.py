e70ea4adf47a54532bd8210c0343720a2881ccc4
salomimi <160702370+salomimi@users.noreply.github.com>
2025-11-10 17:42:32 +0000

    colors

---
+++ 4.4/main.py
@@ -0,0 +1,24 @@
+# Title:
+# Author: Salomi Aiyathurai
+
+import turtle
+
+recursive_drawings = {
+    "tree": {}
+    "snowflake": {}
+}
+
+themes = {
+    # tree themes
+    "original_tree": {"branch": "brown", "leaf": "green"}
+    "red": {"light red": "#FFA07A", "dark red": "#8B0000"}
+    "orange": {"light orange": "#FFD580", "dark orange": "#FF8C00"}
+    "yellow": {"light yellow": "#FFFF99", "dark yellow": "#FFD700"}
+    "green": {"light green": "#90EE90", "dark green": "#006400"}
+    "brown": {"light brown": "#CF8E39", "dark brown": "#8B4513"}
+
+    # snowflake themes
+    "original_snowflake": {"light gray": "#E0E0E0", "dark gray": "#404040"}
+    "icy": {"light blue": "#E0FFFF", "dark blue": "#1E90FF"}
+    "frost": {"light frost": "#F0F8FF", "dark frost": "#4682B4"}
+}
